72a30bf189c81e252809e02ba72e963a07532ff4
soringuzun <46331853+soringuzun@users.noreply.github.com>
2021-01-15 22:49:25 +0200

    Update run-tests-in-virtual-screen.sh

--- bin/run-tests-in-virtual-screen.sh
+++ bin/run-tests-in-virtual-screen.sh
@@ -2,13 +2,13 @@
 
 HOME=${ROBOT_WORK_DIR}
 
-if [ "${ROBOT_TEST_RUN_ID}" = "" ]
+if [ "${ROBOT_TEST_RUN_ID}" = "" ];
 then
     ROBOT_REPORTS_FINAL_DIR="${ROBOT_REPORTS_DIR}"
 else
     REPORTS_DIR_HAS_TRAILING_SLASH=`echo ${ROBOT_REPORTS_DIR} | grep '/$'`
 
-    if [ ${REPORTS_DIR_HAS_TRAILING_SLASH} -eq 0 ]
+    if [ ${REPORTS_DIR_HAS_TRAILING_SLASH} -eq 0 ];
     then
         ROBOT_REPORTS_FINAL_DIR="${ROBOT_REPORTS_DIR}${ROBOT_TEST_RUN_ID}"
     else
@@ -20,7 +20,7 @@ fi
 mkdir -p ${ROBOT_REPORTS_FINAL_DIR}
 
 # No need for the overhead of Pabot if no parallelisation is required
-if [ $ROBOT_THREADS -eq 1 ]
+if [ $ROBOT_THREADS -eq 1 ];
 then
     xvfb-run \
         --server-args="-screen 0 ${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_COLOUR_DEPTH} -ac" \
@@ -40,7 +40,7 @@ else
         $ROBOT_TESTS_DIR
 fi
 
-if [ ${AWS_UPLOAD_TO_S3} = true ]
+if [ ${AWS_UPLOAD_TO_S3} = true ];
 then
     echo "Uploading report to AWS S3..."
     aws s3 sync $ROBOT_REPORTS_FINAL_DIR/ s3://${AWS_BUCKET_NAME}/robot-reports/
